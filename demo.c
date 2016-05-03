1deac8a7d144b9c82d887962f719a8827d02ba78
Timm Bäder <mail@baedert.org>
2016-05-03 17:00:03 +0200

    Foo

--- demo.c
+++ demo.c
@@ -1,5 +1,6 @@
 #include <gtk/gtk.h>
 #include "gd-model-list-box.h"
+#include "pnl-multi-paned.h"
 
 /* Test class {{{ */
 struct _GdData
@@ -27,6 +28,8 @@ struct _GdRowWidget
 {
   GtkBox parent_instance;
 
+  GtkWidget *paned;
+
   GtkWidget *label1;
   GtkWidget *label2;
   GtkWidget *button;
@@ -44,20 +47,26 @@ static GtkWidget * gd_row_widget_new (void) { return GTK_WIDGET (g_object_new (G
 static void gd_row_widget_init (GdRowWidget *d)
 {
   gtk_box_set_spacing (GTK_BOX (d), 6);
+  gtk_widget_set_margin_start (GTK_WIDGET (d), 12);
+  gtk_widget_set_margin_end (GTK_WIDGET (d), 12);
+
+  d->paned = pnl_multi_paned_new (GTK_ORIENTATION_HORIZONTAL);
+
 
   d->label1 = gtk_label_new ("");
   d->label2 = gtk_label_new ("");
   d->button = gtk_button_new_with_label ("Click Me");
 
   gtk_label_set_ellipsize (GTK_LABEL (d->label2), TRUE);
+  gtk_widget_set_hexpand (d->label2, TRUE);
 
-  gtk_container_add (GTK_CONTAINER (d), d->label1);
-  gtk_container_add (GTK_CONTAINER (d), d->label2);
-  gtk_container_add (GTK_CONTAINER (d), d->button);
 
-  gtk_widget_show (d->label1);
-  gtk_widget_show (d->label2);
-  gtk_widget_show (d->button);
+  gtk_container_add (GTK_CONTAINER (d->paned), d->label1);
+  gtk_container_add (GTK_CONTAINER (d->paned), d->label2);
+  gtk_container_add (GTK_CONTAINER (d->paned), d->button);
+  gtk_container_add (GTK_CONTAINER (d), d->paned);
+
+  gtk_widget_show_all (GTK_WIDGET (d));
 }
 static void gd_row_widget_class_init (GdRowWidgetClass *dc) {}
 /* }}} */
@@ -74,8 +83,11 @@ fill_func (gpointer   item,
            gpointer   user_data)
 {
   GdRowWidget *row;
+  GtkWidget *header = user_data;
   GdData *data = GD_DATA (item);
   gchar *label;
+
+  g_assert (header);
 
   if (G_UNLIKELY (!old_widget))
     {
@@ -101,7 +113,6 @@ fill_func (gpointer   item,
   return GTK_WIDGET (row);
 }
 
-
 int
 main (int argc, char **argv)
 {
@@ -111,13 +122,18 @@ main (int argc, char **argv)
 
   GtkWidget *window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
   GtkWidget *scroller = gtk_scrolled_window_new (NULL, NULL);
+  GtkWidget *box = gtk_box_new (GTK_ORIENTATION_VERTICAL, 0);
   GtkWidget *list = gd_model_list_box_new ();
+  GtkWidget *header = pnl_multi_paned_new (GTK_ORIENTATION_HORIZONTAL);
+  GtkWidget *column_header1 = gtk_label_new ("Column1");
+  GtkWidget *column_header2 = gtk_label_new ("Column2");
+  GtkWidget *column_header3 = gtk_label_new ("Column3");
 
   size_group1 = gtk_size_group_new (GTK_SIZE_GROUP_HORIZONTAL);
   size_group2 = gtk_size_group_new (GTK_SIZE_GROUP_HORIZONTAL);
 
   GListStore *store = g_list_store_new (GD_TYPE_DATA);
-  model_size = 1500000;
+  model_size = 15000;
   for (i = 0; i < model_size; i ++)
     {
       GdData *d = g_object_new (GD_TYPE_DATA, NULL);
@@ -129,14 +145,20 @@ main (int argc, char **argv)
 
 
   gd_model_list_box_set_model (GD_MODEL_LIST_BOX (list), G_LIST_MODEL (store));
-  gd_model_list_box_set_fill_func (GD_MODEL_LIST_BOX (list), fill_func, NULL);
+  gd_model_list_box_set_fill_func (GD_MODEL_LIST_BOX (list), fill_func, header);
 
+  gtk_container_add (GTK_CONTAINER (header), column_header1);
+  gtk_container_add (GTK_CONTAINER (header), column_header2);
+  gtk_container_add (GTK_CONTAINER (header), column_header3);
+  gtk_container_add (GTK_CONTAINER (box), header);
 
+  gtk_widget_set_vexpand (scroller, TRUE);
   gtk_container_add (GTK_CONTAINER (scroller), list);
-  gtk_container_add (GTK_CONTAINER (window), scroller);
+  gtk_container_add (GTK_CONTAINER (box), scroller);
+  gtk_container_add (GTK_CONTAINER (window), box);
 
   g_signal_connect (G_OBJECT (window), "delete-event", G_CALLBACK (gtk_main_quit), NULL);
-  gtk_window_resize (GTK_WINDOW (window), 200, 200);
+  gtk_window_resize (GTK_WINDOW (window), 500, 200);
   gtk_widget_show_all (window);
   gtk_main ();
   return 0;
